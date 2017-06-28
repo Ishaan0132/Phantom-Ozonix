2bce3c9ecb82803fc50d25fdbef580f1a9a9d196
sirDonovan <quivistis@gmail.com>
2017-06-28 02:41:35 -0500

    Test: run the Typescript compiler

--- test/main.js
+++ test/main.js
@@ -2,6 +2,10 @@
 
 require('./../app.js');
 
+// tsc is run this way to allow the default config.js file to be created
+const execSync = require('child_process').execSync;
+execSync('tsc', {stdio: 'inherit'});
+
 const room = Rooms.add('mocha');
 
 /**
