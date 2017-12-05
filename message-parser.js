d1cd5d56e2abfa93e85c697a11fa826d1e07d992
sirDonovan <quivistis@gmail.com>
2017-12-04 18:11:14 -0600

    MessageParser: pass original commands in run()

--- message-parser.js
+++ message-parser.js
@@ -26,13 +26,15 @@ class Context {
 	 * @param {Room | User} room
 	 * @param {User} user
 	 * @param {string} command
+	 * @param {string} originalCommand
 	 * @param {number} [time]
 	 */
-	constructor(target, room, user, command, time) {
+	constructor(target, room, user, command, originalCommand, time) {
 		this.target = target ? target.trim() : '';
 		this.room = room;
 		this.user = user;
 		this.command = command;
+		this.originalCommand = originalCommand;
 		this.time = time || Date.now();
 	}
 
@@ -78,33 +80,35 @@ class Context {
 	}
 
 	/**
-	 * @param {string} [command]
-	 * @param {string} [target]
+	 * @param {string} [newCommand]
+	 * @param {string} [newTarget]
 	 * @returns {boolean}
 	 */
-	run(command, target) {
-		if (command) {
-			command = Tools.toId(command);
-			if (!Commands[command]) return false;
-			if (typeof Commands[command] === 'string') {
+	run(newCommand, newTarget) {
+		let command = this.command;
+		let target = this.target;
+		let originalCommand = this.originalCommand;
+		if (newCommand) {
+			newCommand = Tools.toId(newCommand);
+			if (!Commands[newCommand]) return false;
+			originalCommand = newCommand;
+			if (typeof Commands[newCommand] === 'string') {
 				// @ts-ignore Typescript bug - issue #10530
-				command = Commands[command];
+				newCommand = Commands[newCommand];
 			}
-			if (target) {
-				target = target.trim();
+			command = newCommand;
+			if (newTarget) {
+				target = newTarget.trim();
 			} else {
 				target = '';
 			}
-		} else {
-			command = this.command;
-			target = this.target;
 		}
 
 		if (typeof Commands[command] !== 'function') return false;
 
 		try {
 			// @ts-ignore Typescript bug - issue #10530
-			Commands[command].call(this, target, this.room, this.user, this.command, this.time);
+			Commands[command].call(this, target, this.room, this.user, originalCommand, this.time);
 		} catch (e) {
 			let stack = e.stack;
 			stack += 'Additional information:\n';
@@ -126,7 +130,7 @@ class MessageParser {
 	constructor() {
 		this.formatsList = [];
 		this.formatsData = {};
-		this.globalContext = new Context('', Rooms.globalRoom, Users.self, '');
+		this.globalContext = new Context('', Rooms.globalRoom, Users.self, '', '');
 	}
 
 	/**
@@ -349,13 +353,14 @@ class MessageParser {
 		}
 		command = Tools.toId(command);
 		if (!Commands[command]) return;
+		let originalCommand = command;
 		if (typeof Commands[command] === 'string') {
 			// @ts-ignore Typescript bug - issue #10530
 			command = Commands[command];
 		}
 		if (typeof Commands[command] !== 'function') return;
 
-		return new Context(target, room, user, command, time).run();
+		return new Context(target, room, user, command, originalCommand, time).run();
 	}
 
 	parseFormats() {
