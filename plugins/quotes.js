19720275c2787267c5487fbad812d27ebe4ffb45
sirDonovan <quivistis@gmail.com>
2018-02-02 00:23:45 -0600

    Add quote and YouTube plugins

---
+++ plugins/quotes.js
@@ -0,0 +1,102 @@
+/**
+ * Quotes
+ * Cassius - https://github.com/sirDonovan/Cassius
+ *
+ * Manages the quotes system for the bot, allowing the adding,
+ * removing, and viewing of added quotes.
+ *
+ * @author Mystifi
+ * @license MIT license
+ */
+
+'use strict';
+
+// These are the valid ranks that a Room Owner can set when
+// modifying the permissions.
+const validRanks = ['+', '%', '@', '#'];
+
+/**
+ * Obtains the given room's database. If the quotes database
+ * wasn't already initialised, then it is done here.
+ * @param {Room | string} room
+ * @return {AnyObject}
+ */
+function getDatabase(room) {
+	// In case a Room object was passed:
+	if (room instanceof Rooms.Room) room = room.id;
+	let database = Storage.getDatabase(room);
+	if (!database.quotes) database.quotes = [];
+	if (!database.defaultRanks) {
+		// This plugin allows the changing of which auth level(s)
+		// have the ability to modify the room's quotes database.
+		// Along with setting a default quotes rank, this allows
+		// other rooms to add default ranks in this object.
+		database.defaultRanks = {};
+		database.defaultRanks['quotes'] = '+';
+	}
+	return database;
+}
+
+/**@type {{[k: string]: Command | string}} */
+let commands = {
+	quotesrank: 'setquotesrank',
+	setquotesrank: function (target, room, user) {
+		if (room instanceof Users.User || !user.hasRank(room, '#')) return;
+		let database = getDatabase(room.id);
+		target = target.trim();
+		if (!target) return this.say("Users of rank " + database.defaultRanks['quotes'] + " and higher can manage room quotes.");
+		if (!validRanks.includes(target)) return this.say("Unknown option. Valid ranks: " + validRanks.join(", "));
+		database.defaultRanks['quotes'] = target;
+		Storage.exportDatabase(room.id);
+		this.say("Users of rank " + target + " and above can now manage room quotes.");
+	},
+	addquote: function (target, room, user) {
+		if (room instanceof Users.User) return;
+		let database = getDatabase(room.id);
+		if (!user.hasRank(room, database.defaultRanks['quotes'])) return;
+		target = target.trim();
+		if (!target) return this.say("Please use the following format: .addquote quote");
+		let quotes = database.quotes;
+		let index = quotes.findIndex(/**@param {string} quote */ quote => Tools.toId(quote) === Tools.toId(target));
+		if (index >= 0) return this.say("That quote already exists.");
+		quotes.push(target);
+		Storage.exportDatabase(room.id);
+		this.say("Your quote was successfully added.");
+	},
+	removequote: function (target, room, user) {
+		if (room instanceof Users.User) return;
+		let database = getDatabase(room.id);
+		if (!user.hasRank(room, database.defaultRanks['quotes'])) return;
+		target = target.trim();
+		if (!target) return this.say("Please use the following format: .removequote quote");
+		let quotes = database.quotes;
+		let index = quotes.findIndex(/**@param {string} quote */ quote => Tools.toId(quote) === Tools.toId(target));
+		if (index < 0) return this.say("Your quote doesn't exist in the database.");
+		quotes.splice(index, 1);
+		Storage.exportDatabase(room.id);
+		this.say("Your quote was successfully removed.");
+	},
+	randquote: function (target, room, user) {
+		if (room instanceof Users.User || !user.hasRank(room, '+')) return;
+		let quotes = getDatabase(room.id).quotes;
+		if (!quotes.length) return this.say("This room doesn't have any quotes.");
+		this.say(Tools.sampleOne(quotes));
+	},
+	quotes: function (target, room, user) {
+		if (room instanceof Users.User || !user.hasRank(room, '+')) return;
+		let quotes = getDatabase(room.id).quotes;
+		if (!quotes.length) return this.say("This room doesn't have any quotes.");
+		let prettifiedQuotes = "Quotes for " + room.id + ":\n\n" + quotes.map(
+			/**
+			 * @param {string} quote
+			 * @param {number} index
+			 */
+			(quote, index) => (index + 1) + ": " + quote
+		).join("\n");
+		Tools.uploadToHastebin(prettifiedQuotes, /**@param {string} hastebinUrl */ hastebinUrl => {
+			this.say("Room quotes: " + hastebinUrl);
+		});
+	},
+};
+
+exports.commands = commands;
---
+++ plugins/youtube.js
@@ -0,0 +1,130 @@
+/**
+ * YouTube
+ * Cassius - https://github.com/sirDonovan/Cassius
+ *
+ * Fetches YouTube video information and handles
+ * adding videos to the database
+ *
+ * @license MIT license
+ */
+
+'use strict';
+
+const https = require('https');
+const youtubeRegex = /https:\/\/(youtu\.be\/|(www\.)?youtube\.com\/watch\?v=)[A-Za-z0-9\-_]{11}/g;
+const linkCooldowns = {};
+
+/**
+ * @param {Room} room
+ * @param {string} messageType
+ * @param {Array<string>} splitMessage
+ */
+function parseMessage(room, messageType, splitMessage) {
+	switch (messageType) {
+	case 'c': {
+		let message = splitMessage.slice(1).join('|');
+		let link = message.match(youtubeRegex);
+		if (link) {
+			for (let i = 0, len = link.length; i < len; i++) {
+				parseYouTubeLink(room, link[i]);
+			}
+		}
+		break;
+	}
+	case 'c:': {
+		let message = splitMessage.slice(2).join('|');
+		let link = message.match(youtubeRegex);
+		if (link) {
+			for (let i = 0, len = link.length; i < len; i++) {
+				parseYouTubeLink(room, link[i]);
+			}
+		}
+		break;
+	}
+	}
+}
+
+exports.parseMessage = parseMessage;
+
+/**
+ * @param {Room} room
+ * @param {string} link
+ */
+function parseYouTubeLink(room, link) {
+	if (Client.requestQueueTimeout) {
+		Client.requestQueue.push(() => parseYouTubeLink(room, link));
+		return;
+	}
+	let now = Date.now();
+	if (link in linkCooldowns && now - linkCooldowns[link] < (2 * 60 * 1000)) return;
+	linkCooldowns[link] = now;
+
+	let requestOptions = {
+		host: 'www.youtube.com',
+		port: 443,
+		path: '/oembed?url=' + encodeURIComponent(link) + '&format=json',
+	};
+	https.get(requestOptions, response => {
+		response.setEncoding('utf8');
+		let data = '';
+		response.on('data', /**@param {string} chunk*/ chunk => {
+			data += chunk;
+		});
+		response.on('end', () => {
+			if (data) {
+				try {
+					let parsed = JSON.parse(data);
+					if (parsed.title) {
+						let title = parsed.title.toLowerCase();
+						for (let i = 0, len = Config.bannedWords.length; i < len; i++) {
+							if (title.includes(Config.bannedWords[i])) return room.say("Unable to fetch title.");
+						}
+						room.say(parsed.title + " - by " + parsed['author_name']);
+					}
+				} catch (e) {}
+			}
+		});
+	}).on('error', /**@param {Error} error*/ error => {
+		console.log('HTTPS error: ' + error.stack);
+	});
+
+	Client.prepareNextRequest();
+}
+
+/**@type {{[k: string]: Command | string}} */
+let commands = {
+	addvideo: function (target, room, user) {
+		if (room instanceof Users.User || !user.hasRank(room, '%')) return;
+		if (room.id !== 'youtube') return;
+		let database = Storage.databases[room.id];
+		if (!database) return;
+		if (!target.includes("youtube.com/watch?v=")) return this.say("You can only add YouTube video links.");
+		let link = target.substr(target.indexOf("youtube.com/watch?v=")).trim();
+		if (!database.youtubeLinks) database.youtubeLinks = [];
+		if (database.youtubeLinks.includes(link)) return this.say("That video is already in the database.");
+		database.youtubeLinks.push(link);
+		this.say("That video has been added to the database.");
+	},
+	removevideo: 'deletevideo',
+	deletevideo: function (target, room, user) {
+		if (room instanceof Users.User || !user.hasRank(room, '%')) return;
+		if (room.id !== 'youtube') return;
+		let database = Storage.databases[room.id];
+		if (!database) return;
+		if (!target.includes("youtube.com/watch?v=")) return this.say("You can only add YouTube video links.");
+		let link = target.substr(target.indexOf("youtube.com/watch?v=")).trim();
+		if (!database.youtubeLinks) database.youtubeLinks = [];
+		if (!database.youtubeLinks.includes(link)) return this.say("That video is not in the database.");
+		database.youtubeLinks.splice(database.youtubeLinks.indexOf(link), 1);
+		this.say("That video has been removed from the database.");
+	},
+	getvideo: function (target, room, user) {
+		if (room instanceof Users.User || !user.hasRank(room, '+')) return;
+		if (room.id !== 'youtube') return;
+		let database = Storage.databases[room.id];
+		if (!database || !database.youtubeLinks) return this.say("There are no videos stored in the database.");
+		this.say("Random stored video: " + Tools.sampleOne(database.youtubeLinks));
+	},
+};
+
+exports.commands = commands;
