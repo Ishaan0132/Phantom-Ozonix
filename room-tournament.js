5b6a6e380f0c093389b7748f5dc5e07783fa576c
A Flying Phantom <35690029+PowerHitter0418@users.noreply.github.com>
2019-04-24 18:27:37 +0530

    Update Information

--- room-tournament.js
+++ room-tournament.js
@@ -1,6 +1,6 @@
 /**
  * Room Tournament
- * Cassius - https://github.com/sirDonovan/Cassius
+ * Phantom Ozonix - https://github.com/PowerHitter0418/Phantom-Ozonix
  *
  * This file contains the tournament and tournament-player classes
  *
