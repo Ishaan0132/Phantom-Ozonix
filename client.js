71e0406c74660299cfbd586b9674e8dbd63dae2b
A Flying Phantom <35690029+PowerHitter0418@users.noreply.github.com>
2019-04-24 18:16:44 +0530

    Update client.js

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 /**
  * Client
- * Cassius - https://github.com/sirDonovan/Cassius
+ * Phantom Ozonix - https://github.com/PowerHitter0418/Phantom-Ozonix
  *
  * This file connects to the server and handles incoming/outgoing messages.
  *
