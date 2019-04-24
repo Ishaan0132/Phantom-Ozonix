55f130b4c36dc97f33411d8fcb5519d917f91072
A Flying Phantom <35690029+PowerHitter0418@users.noreply.github.com>
2019-04-24 18:05:58 +0530

    Update team.js

--- games/modes/team.js
+++ games/modes/team.js
@@ -1,6 +1,6 @@
 /**
  * Team
- * Cassius - https://github.com/sirDonovan/Cassius
+ * Phantom Ozonix - https://github.com/PowerHitter0418/Phantom-Ozonix
  *
  * This file contains code for the game mode Team
  *
