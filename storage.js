f3df76d7c8511141ae881ece92866690a01e31c6
A Flying Phantom <35690029+FlyingPhantom@users.noreply.github.com>
2020-04-09 11:35:04 +0530

    Rename

--- storage.js
+++ storage.js
@@ -1,6 +1,6 @@
 /**
  * Storage
- * Phantom Ozonix - https://github.com/PowerHitter0418/Phantom-Ozonix
+ * Phantom Ozonix - https://github.com/FlyingPhantom/Phantom-Ozonix
  *
  * This file handles the storage of room databases
  *
