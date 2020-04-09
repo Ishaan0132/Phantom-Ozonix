462c616c9b7f400cb3720d86b0e84c5872124247
A Flying Phantom <35690029+FlyingPhantom@users.noreply.github.com>
2020-04-09 11:36:34 +0530

    Rename

--- users.js
+++ users.js
@@ -1,6 +1,6 @@
 /**
  * Users
- * Phantom Ozonix - https://github.com/PowerHitter0418/Phantom-Ozonix
+ * Phantom Ozonix - https://github.com/FlyingPhantom/Phantom-Ozonix
  *
  * This file tracks information about users.
  *
