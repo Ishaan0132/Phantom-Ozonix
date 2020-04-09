1f83ba85f5e7d6270d912fec9f8837d792784389
A Flying Phantom <35690029+FlyingPhantom@users.noreply.github.com>
2020-04-09 11:32:55 +0530

    Rename

--- message-parser.js
+++ message-parser.js
@@ -1,6 +1,6 @@
 /**
  * Message Parser
- * Phantom Ozonix - https://github.com/PowerHitter0418/Phantom-Ozonix
+ * Phantom Ozonix - https://github.com/FlyingPhantom/Phantom-Ozonix
  *
  * This file parses messages sent by the server.
  *
