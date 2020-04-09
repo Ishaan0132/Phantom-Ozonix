a3c27a1bb47b3521563069ffa126047303afbfa1
A Flying Phantom <35690029+FlyingPhantom@users.noreply.github.com>
2020-04-09 11:31:09 +0530

    Rename

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 /**
  * Client
- * Phantom Ozonix - https://github.com/PowerHitter0418/Phantom-Ozonix
+ * Phantom Ozonix - https://github.com/FlyingPhantom/Phantom-Ozonix
  *
  * This file connects to the server and handles incoming/outgoing messages.
  *
