5248146908e48d7b0b47041f81197efd2f283f25
A Flying Phantom <35690029+FlyingPhantom@users.noreply.github.com>
2020-04-09 11:23:48 +0530

    Renames

--- games/anagrams.js
+++ games/anagrams.js
@@ -1,6 +1,6 @@
 /**
  * Anagrams game
- * Phantom Ozonix - https://github.com/PowerHitter0418/Phantom-Ozonix
+ * Phantom Ozonix - https://github.com/FlyingPhantom/Phantom-Ozonix
  *
  * @license MIT license
  */
