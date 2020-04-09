c4b4d7658c00b3cef2123a3cc1850befd7755937
A Flying Phantom <35690029+FlyingPhantom@users.noreply.github.com>
2020-04-09 11:36:14 +0530

    Rename

--- tournaments.js
+++ tournaments.js
@@ -1,6 +1,6 @@
 /**
  * Tournaments
- * Phantom Ozonix - https://github.com/PowerHitter0418/Phantom-Ozonix
+ * Phantom Ozonix - https://github.com/FlyingPhantom/Phantom-Ozonix
  *
  * This file contains the tournaments manager
  *
