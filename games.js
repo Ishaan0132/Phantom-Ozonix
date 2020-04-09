c33e96a4bda8d1e17df4e62402bb6a3bf2435ff6
A Flying Phantom <35690029+FlyingPhantom@users.noreply.github.com>
2020-04-09 11:32:28 +0530

    Rename

--- games.js
+++ games.js
@@ -1,6 +1,6 @@
 /**
  * Games
- * Phantom Ozonix - https://github.com/PowerHitter0418/Phantom-Ozonix
+ * Phantom Ozonix - https://github.com/FlyingPhantom/Phantom-Ozonix
  *
  * This file contains the games manager
  *
