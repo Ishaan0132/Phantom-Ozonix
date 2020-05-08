4c13461570542747cf20091d6839cb6c5ef46383
A Flying Phantom <35690029+FlyingPhantom@users.noreply.github.com>
2020-05-08 14:35:13 +0530

    Commands: Edits

--- commands.js
+++ commands.js
@@ -111,11 +111,11 @@ let commands = {
 		this.say("Random pick: " + pick);
 	},
         iq: function (target, room, user) {
-	if (!(room instanceof Users.User) && !user.hasRank(room, '+')) return;
-        if (!target) return this.say('You didn\'t specify a person');
-        this.say('Analysisng the IQ of the person. ' + 'Give me a few moments.......')
-        var x = Math.floor((Math.random() * 200) + 1);
-        this.say('The iq of ' + target + ' is :  ' +   x );
+	    if (!(room instanceof Users.User) && !user.hasRank(room, '+')) return;
+            if (!target) return this.say('You didn\'t specify a person');
+            this.say('Analysisng the IQ of the person. ' + 'Give me a few moments.......')
+            var x = Math.floor((Math.random() * 200) + 1);
+            this.say('The iq of ' + target + ' is :  ' +   x );
         },
 	 ping: function (target, room, user) {
           if(!(room instanceof Users.User) && !user.hasRank(room, '+')) return;
@@ -133,68 +133,68 @@ let commands = {
         }},
 	j: 'judge',
         judge:  function (target, room, user) {
-	if (!(room instanceof Users.User) && !user.hasRank(room, '+')) return;
-        var judgement = [" is so cute"," is the worst!!!"," is um eh not bad "," is the best"," is ok"];
-        var rand = Math.floor((Math.random() * 4) + 1); 
-        if (!["!", "/"].includes(target.charAt(0))) 
-        this.say(target.split('/') + judgement[rand]);
+	        if (!(room instanceof Users.User) && !user.hasRank(room, '+')) return;
+                var judgement = [" is so cute"," is the worst!!!"," is um eh not bad "," is the best"," is ok"];
+                var rand = Math.floor((Math.random() * 4) + 1); 
+                if (!["!", "/"].includes(target.charAt(0))) 
+               this.say(target.split('/') + judgement[rand]);
         },
-        timer: function(target, room, user) {
-	if (!(room instanceof Users.User) && !user.hasRank(room, '+')) return;
-	const id = Tools.toId(target);
-	if (id === 'off' || id === 'end') {
-	if (!room.timers || !(user.id in room.timers)) return this.say("You do not have a timer running.");
-	clearTimeout(room.timers[user.id]);
-	delete room.timers[user.id];
-	return this.say("Your timer has been turned off.");
-	}
+      timer: function(target, room, user) {
+			if (!user.hasRank(room, 'voice') && !user.isDeveloper) return;
+			const id = Tools.toId(target);
+			if (id === 'off' || id === 'end') {
+				if (!room.timers || !(user.id in room.timers)) return this.say("You do not have a timer running.");
+				clearTimeout(room.timers[user.id]);
+				delete room.timers[user.id];
+				return this.say("Your timer has been turned off.");
+			}
 
-	let time;
-	if (id.length === 1) {
-	time = parseInt(id) * 60;
-	} else {
-	time = parseInt(id);
-	}
-	if (isNaN(time) || time > 1800 || time < 5) return this.say("Please enter an amount of time between 5 seconds and 30 minutes.");
-	time *= 1000;
+			let time;
+			if (id.length === 1) {
+				time = parseInt(id) * 60;
+			} else {
+				time = parseInt(id);
+			}
+			if (isNaN(time) || time > 1800 || time < 5) return this.say("Please enter an amount of time between 5 seconds and 30 minutes.");
+			time *= 1000;
 
-	if (!room.timers) room.timers = {};
-	if (user.id in room.timers) clearTimeout(room.timers[user.id]);
-	room.timers[user.id] = setTimeout(() => {
-	room.say(user.name + ": time is up!");
-	delete room.timers[user.id];
-	}, time);
-	this.say("Your timer has been set for: " + Tools.toDurationString(time) + ".");
+			if (!room.timers) room.timers = {};
+			if (user.id in room.timers) clearTimeout(room.timers[user.id]);
+			room.timers[user.id] = setTimeout(() => {
+				room.say(user.name + ": time is up!");
+				delete room.timers[user.id];
+			}, time);
+			this.say("Your timer has been set for: " + Tools.toDurationString(time) + ".");
 		},
         cal : 'calculate',
         calculate: function(target, room, user){
-	if (!(room instanceof Users.User) && !user.hasRank(room, '+')) return;
-        let alphabets = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z'];
-        let cond = true;
-        for(let i = 0;i < alphabets.length;i++){
-        if(target.includes(alphabets[i])) cond = false;
-        }
-        if(cond == true) return this.say(eval(target));
+	           if (!(room instanceof Users.User) && !user.hasRank(room, '+')) return;
+                   let alphabets = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z'];
+                   let cond = true;
+                   for(let i = 0;i < alphabets.length;i++){
+                   if(target.includes(alphabets[i])) cond = false;
+                }
+                   if(cond == true) return this.say(eval(target));
         },
         reversio: function(target, room, user){
-        let str = target;
-        var n = str.includes("!");
-        if(n)
-       {
-	return this.say("You cant use ! in your sentence");
-       }
-        var m = str.includes("/");
-        if(m)
-       {
-	return this.say("You cant use / in your sentence");
-       }
-       var splitString = str.split("");
-       var reverseArray = splitString.reverse();
-       var joinArray = reverseArray.join("");
+                  let str = target;
+                  var n = str.includes("!");
+                  if(n)
+                 {
+	               return this.say("You cant use ! in your sentence");
+                 }
+                 var m = str.includes("/");
+                 if(m)
+                 {
+	               return this.say("You cant use / in your sentence");
+                 }
+                 var splitString = str.split("");
+                 var reverseArray = splitString.reverse();
+                 var joinArray = reverseArray.join("");
     
-       if(joinArray == target) {
-        return this.say("You spotted a Palindrome! " + joinArray);}
-        return this.say(joinArray);
+                if(joinArray == target) {
+                 return this.say("You spotted a Palindrome! " + joinArray);}
+                 return this.say(joinArray);
        },
 	joke: function(target, room, user) {
         if (!(room instanceof Users.User) && !user.hasRank(room, '+')) return;
@@ -261,7 +261,7 @@ let commands = {
 	"randpoke": "randompokemon",
         "randp": "randompokemon",
         randompokemon: function(target, room, user) {
-	                if (!Users.self.hasRank(room, '+') || (!user.hasRank(room, '+'))) return;
+	                if (room instanceof Users.User && !user.hasRank(room, '+')) return;
 	                if (!target) {
        	                const species = Tools.getExistingPokemon(Tools.sampleOne(Object.keys(Tools.data.pokedex))).name;
 	                {
@@ -274,21 +274,21 @@ let commands = {
         "rmove": "randommove",
 	"randmove": "randommove",
          randommove:  function(target, room, user) {
-			if (!Users.self.hasRank(room, '+') || (!user.hasRank(room, '+'))) return;
+			if (room instanceof Users.User && !user.hasRank(room, '+')) return;
 			const move = Tools.getExistingMove(Tools.sampleOne(Object.keys(Tools.data.moves))).name;
   		        this.say('!dt ' + move);
         },
         "ritem": "randomitem",
 	"randitem": "randomitem",
          randomitem: function (target, room, user) {
-		     if (!Users.self.hasRank(room, '+') || (!user.hasRank(room, '+'))) return;
+		     if (room instanceof Users.User && !user.hasRank(room, '+')) return;
 		     const item = Tools.getExistingItem(Tools.sampleOne(Object.keys(Tools.data.items))).name;
 		     this.say('!dt ' + item);
 	},
         "rability": "randomability",
 	"randability": "randomability",
          randomability: function(target, room, user) {
-			if (!Users.self.hasRank(room, '+') || (!user.hasRank(room, '+'))) return;
+			if (room instanceof Users.User && !user.hasRank(room, '+')) return;
 			const abilities = Object.keys(Tools.data.abilities);
 			let ability = Tools.getExistingAbility(Tools.sampleOne(abilities));
 			while (ability.id === 'noability') {
@@ -299,7 +299,7 @@ let commands = {
 	"rtype": "randomtype",
 	"randtype": "randomtype",
          randomtype:  function(target, room, user) {
-			if (!Users.self.hasRank(room, '+') || (!user.hasRank(room, '+'))) return;
+			if (room instanceof Users.User && !user.hasRank(room, '+')) return;
 			const types = Object.keys(Tools.data.typeChart);
 			let type = Tools.sampleOne(types);
 			if (Tools.random(2)) {
@@ -311,14 +311,14 @@ let commands = {
         "randchar": "randomcharacter",
         "rchar": "randomcharacter",
         randomcharacter: function (target, room, user) {
-			if (!Users.self.hasRank(room, '+') || (!user.hasRank(room, '+'))) return;
+			if (room instanceof Users.User && !user.hasRank(room, '+')) return;
 			this.say('Randomly generated character: **' + Tools.sampleOne(Tools.data.characters).trim() + '**');
 	},
         "randlocation": "randomlocation",
         "rloc": "randomlocation",
 	"randloc": "randomlocation",
         randomlocation:	function(target, room, user) {
-			if (!Users.self.hasRank(room, '+') || (!user.hasRank(room, '+'))) return;
+			if (room instanceof Users.User && !user.hasRank(room, '+')) return;
 			this.say('Randomly generated location: **' + Tools.sampleOne(Tools.data.locations).trim() + '**');
 	}, 
 
