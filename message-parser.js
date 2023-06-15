b3c9432b0c4781a1088af8f54a467291e0543c1e
Ishaan Shaikh <35690029+Ishaan0132@users.noreply.github.com>
2023-06-15 11:45:07 +0530

    Update message-parser.js

--- message-parser.js
+++ message-parser.js
@@ -1,6 +1,6 @@
 /**
  * Message Parser
- * Phantom Ozonix - https://github.com/FlyingPhantom/Phantom-Ozonix
+ * Phantom Ozonix - https://github.com/Ishaan0132/Phantom-Ozonix
  *
  * This file parses messages sent by the server.
  *
