260c46050cb260d76b27df218eeab92a14d58db9
Ishaan Shaikh <35690029+Ishaan0132@users.noreply.github.com>
2023-06-15 11:45:57 +0530

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,6 +1,6 @@
 /**
  * Storage
- * Phantom Ozonix - https://github.com/FlyingPhantom/Phantom-Ozonix
+ * Phantom Ozonix - https://github.com/Ishaan0132/Phantom-Ozonix
  *
  * This file handles the storage of room databases
  *
